ba02be45ad0709b887c058ca956f1b3fe4faaed2
Ayoubrrj <ayoubberrajaapro@gmail.com>
2025-12-03 18:00:34 +0100

    Mise en place de l'api + affichage des produits

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <script setup>
 import AppHeader from "@/components/AppHeader.vue";
 import Cart from "@/components/cart/Cart.vue";
-import PorductList from "@/components/product/ProductList.vue";
+import ProductList from "@/components/product/ProductList.vue";
 import AppFooter from "@/components/AppFooter.vue";
 </script>
 
 <template>
     <AppHeader />
     <main class="container mx-auto py-8 px-4 flex flex-wrap">
-        <PorductList />
+        <ProductList apiURL="https://68db8534445fdb39dc25a900.mockapi.io/" />
 
         <Cart />
     </main>
--- src/components/product/ProductList.vue
+++ src/components/product/ProductList.vue
@@ -1,15 +1,26 @@
 <script setup>
-import Porduct from "@/components/product/Product.vue";
+import Product from "@/components/product/Product.vue";
+import { productsStore } from "@/stores/products";
+import { onMounted } from "vue";
+
+const props = defineProps({
+    apiURL: { type: String, required: true },
+});
+
+onMounted(async () => {
+    productsStore.init(props.apiURL);
+});
 </script>
 
 <template>
     <section class="w-full md:w-2/3 px-4 mb-8">
-        <!-- Products Section -->
         <h1 class="text-3xl font-bold mb-4">Nouveaux produits</h1>
         <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-            <Porduct />
+            <Product
+                v-for="product in productsStore.products"
+                :key="product.id"
+                :product="product"
+            />
         </div>
     </section>
 </template>
-
-<style scoped></style>
