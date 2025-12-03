8716230b1870d427de98f3cbb1b9c0b55e3dd52b
Ayoubrrj <ayoubberrajaapro@gmail.com>
2025-12-03 16:04:37 +0100

    Mise en place du template en component

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import AppHeader from "@/components/AppHeader.vue";
+import Cart from "@/components/cart/Cart.vue";
+import PorductList from "@/components/product/ProductList.vue";
+import AppFooter from "@/components/AppFooter.vue";
+</script>
+
+<template>
+    <AppHeader />
+    <main class="container mx-auto py-8 px-4 flex flex-wrap">
+        <PorductList />
+
+        <Cart />
+    </main>
+    <AppFooter />
+</template>
+
+<style scoped></style>
---
+++ src/components/product/ProductList.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Porduct from "@/components/product/Product.vue";
+</script>
+
+<template>
+    <section class="w-full md:w-2/3 px-4 mb-8">
+        <!-- Products Section -->
+        <h1 class="text-3xl font-bold mb-4">Nouveaux produits</h1>
+        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+            <Porduct />
+        </div>
+    </section>
+</template>
+
+<style scoped></style>
